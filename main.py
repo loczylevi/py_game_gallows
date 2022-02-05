bcc04406fb07e9177a68c8f0b39dc2b84a7d1d05
loczylevi <71691777+loczylevi@users.noreply.github.com>
2022-02-05 10:27:30 +0000

    frissitések

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ szavak =  ["tank","almafa","villámháború","körte","bojler"]
 hosz = len(szavak[0])
 elet = 9
 tipp = 0
-kvalaszt = random.choice(szavak)
+kivalaszt = random.choice(szavak)
 eltalalt = []
 szeletelo = []
-for betu in szavak[0]:
+for betu in kivalaszt:
     szeletelo.append(betu)
 
 print("Gondoltam egy szóra találd ki melyik az!")
@@ -18,36 +18,34 @@ if kerdes == "i":
   except:
     print("Számot kértem töki!")
     print("Ha már ilyen vicces hangulatban vagy akkor kevesebb életed lesz! :)")
-    elet2 = 2
+    elet2 = 3
 elif kerdes != "n" or kerdes != "i":
   print("Nincs ilyen opció, akkor ennyi életed lesz: 10")
-  elet2 = 9
+  elet2 = 10
 else:
-  elet2 = 9
-print(f"Ennyi probálkozásod van akkor alapból: {elet2+1}")
+  elet2 = 10
+print(f"Ennyi probálkozásod van akkor alapból: {elet2}")
 print("________________________________________\n")
 
 while True:
-    bekeres = input("kerek egy betut ")
+    bekeres = input("kérek egy betüt!\t")
     kereso = [szo for szo in szeletelo if bekeres == szo]
-    if elet == 0:
-        print(f"Sajnálom elfogytak az életeid Vége a játéknak! A szó amire gondoltam: {szavak[0]}")
+    if elet == 0 or elet2 == 0:
+        print(f"Sajnálom elfogytak az életeid Vége a játéknak! A szó amire gondoltam: {kivalaszt}")
         print("<<< GAME OVER >>>")
         break
     if len(kereso) > 0:
         tipp = tipp + 1
         szeletelo.remove(kereso[0])
         eltalalt.append(kereso[0])
-        print(f"Ez a betü: \"{bekeres}\" szerepel a szóban")
-        print(f"jo valaszok: {eltalalt}")
+        print(f"Helyes! Ez a betü: \"{bekeres}\" szerepel a szóban")
+        print(f"Az eltalált betük eddig: {eltalalt}")
     else:
         elet = elet - 1
-        print(f"Ez a betü: \"{bekeres}\" NEM szerepel a szóban")
-        print(f"jo valaszok: {eltalalt}")
-    if tipp == hosz:
-        print("nyertél!!")
-        break
-    if elet == 0:
-        print(f"élet egyenlő: {elet} GAME OVER")
+        elet2 = elet2 - 1
+        print(f"Nem, Ez a betű: \"{bekeres}\" NEM szerepel a szóban!")
+        print(f"A jo betük eddig: {eltalalt}")
+    if tipp == len(szeletelo):
+        print(f"Gratulálok, eltaláltad! A szó amire gondoltam: {kivalaszt}")
         break
 
